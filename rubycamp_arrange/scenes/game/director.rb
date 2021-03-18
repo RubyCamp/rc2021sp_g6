4c777249c41688a90ad4e8284085bd4fb07d5c32
KumagaiKento68 <44947161+KumagaiKento68@users.noreply.github.com>
2021-03-17 20:13:07 -0900

    Add files via upload
    3/18 14:12更新

---
+++ rubycamp_arrange/scenes/game/director.rb
@@ -0,0 +1,50 @@
+# マップエディタモジュール
+module Game
+  # シーン管理用ディレクタークラス
+  class Director < DirectorBase
+    DEBUG_MODE = true
+
+    # 初期化
+    def initialize
+      player_img =  Image.load("images/player.png")
+      @map = Map.new(50, 50, 2, 5, 15)
+      @map.set_scroll_direction(1, 1)
+      @player = Player.new(10, 10, player_img, @map)
+      @font = Font.new(28)
+      @debug_box = RenderTarget.new(32, 32, C_YELLOW)
+    end
+
+    # Scene遷移時に自動呼出しされる規約メソッド
+    def reload
+      @map.reload_map_array
+    end
+
+    # 1フレーム描画
+    def play
+      @debug_boxes = []
+
+      if Input.key_push?(K_SPACE)
+        @player.start_jump
+      end
+
+      @map.update
+      @map.draw
+      @debug_boxes += @player.update(Input.x)
+      @player.draw
+      title_draw
+
+      if DEBUG_MODE
+        @debug_boxes.each do |pos|
+          Window.draw(pos[0], pos[1], @debug_box)
+        end
+      end
+    end
+
+    private
+
+    # タイトル文字列描画
+    def title_draw
+      Window.draw_font(50, 5, "Sample Game", @font)
+    end
+  end
+end
